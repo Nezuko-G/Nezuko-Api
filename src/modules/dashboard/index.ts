540f53aa0b0a96ca5450f956715935f1fa96ccce
Ahmed Aly👾 <ahmedalinaguib33@gmail.com>
2026-04-20 21:43:04 +0200

    feat: add leave request router to global routes for leave management

--- src/modules/dashboard/index.ts
+++ src/modules/dashboard/index.ts
@@ -3,6 +3,7 @@ import { AuthRouter } from "../auth";
 import { BookingDemoRequestRouter } from "../booking-demo-request";
 import { CompanyRouter } from "../company";
 import { EmployeeRouter } from "../employee";
+import { LeaveRequestRouter } from "../leave-request";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", AuthRouter);
 router.use("/booking-demo-request", BookingDemoRequestRouter);
 router.use("/company", CompanyRouter);
 router.use("/employee", EmployeeRouter);
+router.use("/leave-requests", LeaveRequestRouter);
 
 export { router as GlobalRouter };
