b41b10e65e3a6e4e68965dcd2c5d8d74a2cd66a6
Ahmed Aly👾 <ahmedalinaguib33@gmail.com>
2026-04-24 15:46:31 +0200

    feat: add InsuranceRouter to the global router

--- src/modules/dashboard/index.ts
+++ src/modules/dashboard/index.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import { AuthRouter } from "../auth";
 import { BookingDemoRequestRouter } from "../booking-demo-request";
+import { InsuranceRouter } from "../insurance";
 import { CompanyRouter } from "../company";
 import { EmployeeRouter } from "../employee";
 import { LeaveRequestRouter } from "../leave-request";
-import { DepartmentRouter }from '../department';
+import { DepartmentRouter } from "../department";
 
 const router = Router();
 
 router.use("/auth", AuthRouter);
 router.use("/booking-demo-request", BookingDemoRequestRouter);
+router.use("/", InsuranceRouter);
 router.use("/company", CompanyRouter);
 router.use("/employee", EmployeeRouter);
 router.use("/leave-requests", LeaveRequestRouter);
