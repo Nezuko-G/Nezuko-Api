32dd21fa4a60ddaeed9d8f8588e47348988c7fd0
Ahmed Aly👾 <ahmedalinaguib33@gmail.com>
2026-04-28 23:49:38 +0200

    feat(attendance): add index file to export attendance modules

---
+++ src/modules/attendance/index.ts
@@ -0,0 +1,4 @@
+export { AttendanceRouter } from "./attendance.routes.js";
+export { attendanceService } from "./attendance.service.js";
+export { attendanceRepository } from "./attendance.repository.js";
+export { attendanceController } from "./attendance.controller.js";
