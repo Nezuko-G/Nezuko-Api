e88e149145344ec8ca80d4a4accc86ad4cf57e06
Ahmed Nagy <ahmednagy25t@gmail.com>
2026-06-16 00:07:15 +0300

    opencores

--- src/shared/config/cores.options.ts
+++ src/shared/config/cores.options.ts
@@ -1,13 +1,15 @@
 const corsOptions = {
-  origin: process.env.ALLOWED_ORIGINS?.split(",") || [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-  ],
+  origin: process.env.ALLOWED_ORIGINS === "*"
+    ? true
+    : process.env.ALLOWED_ORIGINS?.split(",") || [
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+      ],
   credentials: true,
   optionsSuccessStatus: 200,
-  maxAge: 86400, // 24 hours
+  maxAge: 86400,
 };
 
 
