be8f48ec6f26ab7871954736ccb85b27c284d25b
Ahmed Nagy <ahmednagy25t@gmail.com>
2026-06-16 00:11:08 +0300

    *

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
-import corsOptions from "@/shared/config/cores.options";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
@@ -51,7 +50,7 @@ app.use((_req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-app.use(cors(corsOptions));
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(timeout("50s"));
 app.use(express.json({
